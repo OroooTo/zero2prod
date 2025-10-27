25429567550a0e681506c30171a0d72a37bb97e1
Clement Vallois <cvallois@hitpart.fr>
2025-10-27 11:18:49 +0100

    3.6 done

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,16 @@ use actix_web::dev::Server;
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
-        App::new().route("/health_check", web::get().to(health_check))
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,4 @@
-use std::{ fmt::format, net::TcpListener };
+use std::net::TcpListener;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
@@ -36,4 +36,30 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16())
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_mising() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_case = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursila_le_guin%40toto.fr", "mising the name"),
+        ("", "missing both name and email")
+    ];
+
+    for (invalid_body, error_message) in test_case {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send().await
+            .expect("Failed to execute request");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
